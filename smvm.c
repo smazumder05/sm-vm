32d2878b57b6d4e268fd783a6c581f802619c8ee
smazumder05 <smazumder@gmail.com>
2022-01-03 12:42:47 -0500

    another change.

--- smvm.c
+++ smvm.c
@@ -82,24 +82,3 @@ uint16_t reg[CNT] = {0};
 //op_ex_f op_ex[NOPS] = {
   //  br, add, ld, st, jsr, and, ldr, str, rti, not, ldi, sti, jmp, res, lea, trap
 //};
-
-/**
-Instruction 	OpCode Hex 	OpCode Bin 	C function 	Comments
-------------------------------------------------------------------------------
- br 	0x0 	0b0000 	 void br(uint16_t i) 	Conditional branch
- add 	0x1 	0b0001 	void and(uint16_t i) 	Used for addition.
- ld 	0x2 	0b0010 	void ld(uint16_t i) 	Load RPC + offset
- st 	0x3 	0b0011 	void st(uint16_t i) 	Store
- jsr 	0x4 	0b0100 	void jsr(uint16_t i) 	Jump to subroutine
- and 	0x5 	0b0101 	void and(uint16_t i) 	Bitwise logical AND
- ldr 	0x6 	0b0110 	void ldr(uint16_t i) 	Load Base+Offset
- str 	0x7 	0b0111 	void str(uint16_t i) 	Store base + offset
- rti 	0x8 	0b1000 	void rti(uint16_t i) 	Return from interrupt (not implemented)
- not 	0x9 	0b1001 	void not(uint16_t i) 	Bitwise complement
- ldi 	0xA 	0b1010 	void ldi(uint16_t i) 	Load indirect
- sti 	0xB 	0b1011 	void sti(uint16_t i) 	Store indirect
- jmp 	0xC 	0b1100 	void jmp(uint16_t i) 	Jump/Return to subroutine
-     	0xD 	0b1101 	  	Unused OpCode
- lea 	0xE 	0b1110 	void lea(uint16_t i) 	Load effective address
- trap 	0xF 	0b1111 	void trap(uint16_t i) 	System trap/call/
- */
